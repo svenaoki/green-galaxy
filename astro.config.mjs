96213248fa767b2548dbaf3ec1580db2fe3d4a32
Sven Markus Schilke <Sven.Schilke@viseca.ch>
2026-06-03 09:35:21 +0200

    pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://svenaoki.github.io",
+  base: '/green-galaxy',
   integrations: [mdx(), sitemap()],
 });
